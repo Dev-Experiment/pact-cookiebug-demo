b8262d408f4c28f97c24c94531985a38270a5b82
achand49 <achand49@ford.com>
2021-03-27 00:03:04 -0400

    remove unused imports

--- src/test/kotlin/com/appu/springrest/pact/GreetingProviderPactSpringJunit5MockMVCTest.kt
+++ src/test/kotlin/com/appu/springrest/pact/GreetingProviderPactSpringJunit5MockMVCTest.kt
@@ -10,38 +10,24 @@ import au.com.dius.pact.provider.junitsupport.loader.PactFolder
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget
 import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider
 import com.appu.springrest.controller.GreetingController
-import com.appu.springrest.service.GreetingService
 import org.apache.http.HttpRequest
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.TestTemplate
 import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.time.OffsetDateTime
 import org.springframework.test.web.servlet.MockMvc
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.mock.mockito.MockBean
 
 
-//@ExtendWith(SpringExtension::class)
 @WebMvcTest(GreetingController::class)
 @Provider("greeting")
 @PactFolder("pacts")
 internal class GreetingProviderPactSpringJunit5MockMVCTest {
     companion object {
         private val endTime: OffsetDateTime = OffsetDateTime.parse("2020-07-08T10:52:20.840Z")
-        private val startTime: OffsetDateTime = endTime.minusDays(7)
     }
-
-
-    @MockBean
-    private lateinit var service:GreetingService
-
     @TestTemplate
     @ExtendWith(PactVerificationSpringProvider::class)
     fun pactVerificationTestTemplate(context: PactVerificationContext) {
@@ -69,6 +55,4 @@ internal class GreetingProviderPactSpringJunit5MockMVCTest {
         testTarget.printRequestResponse=true
         context.target = testTarget
     }
-
-
 }
